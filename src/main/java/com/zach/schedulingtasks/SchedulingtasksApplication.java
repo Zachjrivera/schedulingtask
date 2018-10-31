9d36414a633887f713966e84c29962bbb4cb7330
Zachjrivera <Zachjrivera@gmail.com>
2018-10-31 13:11:04 -0400

    Complete

---
+++ src/main/java/com/zach/schedulingtasks/SchedulingtasksApplication.java
@@ -0,0 +1,18 @@
+package com.zach.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulingtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingtasksApplication.class, args);
+
+
+	}
+
+
+}
